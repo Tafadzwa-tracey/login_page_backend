44418ef709f83d578889d801e64bcc1ecf744e78
unknown <tafadzwatracey@gmail.com>
2020-12-24 12:09:08 +0800

    fist commit

---
+++ src/main/java/Tracey/login_page/LoginPageApplication.java
@@ -0,0 +1,13 @@
+package Tracey.login_page;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LoginPageApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LoginPageApplication.class, args);
+	}
+
+}
